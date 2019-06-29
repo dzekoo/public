6bd822915801e43b391c2ba182bbb45665d0aee5
Dzeko <aakhan@Adils-MacBook-Pro.local>
2019-06-29 21:12:28 +0530

    Added main method for invocation

--- src/com/assignment/Main.java
+++ src/com/assignment/Main.java
@@ -1,7 +1,30 @@
 package com.assignment;
 
+import com.assignment.Jobs.JobsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<String> jobs = new ArrayList<>();
+        jobs.add("a => ");
+        jobs.add("b => c");
+        jobs.add("c => f");
+        jobs.add("d => a");
+        jobs.add("e => b");
+        jobs.add("f => ");
+        try {
+            JobsManager jobsManager = new JobsManager(jobs);
+            List<Character> result = jobsManager.getJobOrder();
+            StringBuilder builder = new StringBuilder(result.size());
+            for(Character ch: result)
+            {
+                builder.append(ch);
+            }
+            System.out.println(builder.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
